c8380fc07c7e3330762162e690e3f2bf58c9e4e6
Furquan-Ad <furquanmohammedadhoni@gmail.com>
2026-04-27 20:59:51 +0530

    fixed schema filename issue

--- backend/model/User.js
+++ backend/model/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const userSchema = require("../schemas/userSchema");
+const userSchema = require("../schemas/UserSchema");
 
 
 module.exports = mongoose.model("AuthUser", userSchema);
